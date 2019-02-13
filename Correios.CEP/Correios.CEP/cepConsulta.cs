45ea31d35021316f611600e166c5192b708d3c9e
Murilo Rocha Pereira <34790144+murilopereirame@users.noreply.github.com>
2019-02-13 17:45:36 -0200

    Add files via upload

---
+++ Correios.CEP/Correios.CEP/cepConsulta.cs
@@ -0,0 +1,165 @@
+﻿// Correios.CEP - API destinada a .NET com objetivo de realizar 
+// a busca de endereços a partir do CEP através do site dos Correios.
+// Copyright (C) 2019 Murilo Rocha Peraira <mrex@tuta.io>
+// Copyright (C) 2019 Pereira Tech <mrex@tuta.io>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+namespace Correios.CEP
+{
+    public class cepConsulta
+    {
+        private string _Cep;
+        private string _Rua;
+        private string _Cidade;
+        private string _Bairro;
+        private string _Estado;
+
+        /// <summary>
+        /// A validação para o CEP permite apenas strings de
+        /// oito nove digitos com ou sem máscara, apenas seguindo
+        /// os seguintes padroes: 00000999 ou 00000-999.
+        /// </summary>
+        /// 
+
+        public string Cep
+        {
+            get
+            {
+                return this._Cep;
+            }
+            set
+            {
+                int dCount = 0;
+
+                if (value.Length != 8 && value.Length != 9)
+                    throw new ArgumentException("O CEP informado é inválido!\nO CEP deve conter 8 dígitos (sem hífen) ou 9 (com hífen)");
+
+                foreach (char c in value)
+                    if (Char.IsDigit(c))
+                        dCount++;
+
+                if (dCount != 8)
+                    throw new ArgumentException("O CEP informado é inválido!");
+
+                this._Cep = value;
+            }
+        }
+
+        /// <summary>
+        /// A validação para o nome da Rua permite apenas strings
+        /// respeitando o limite máximo de 500 caracteres.        
+        /// </summary>
+        /// 
+        public string Rua
+        {
+            get
+            {
+                return this._Rua;
+            }
+            set
+            {
+                if (value.Length > 500)
+                    throw new ArgumentException("O nome da rua não pode exceder 500 caracteres!");
+
+                this._Rua = value;
+            }
+        }
+
+        /// <summary>
+        /// A validação para o nome da cidade permite apenas strings
+        /// respeitando o limite máximo de 30 caracteres.
+        /// De acordo com o Google,
+        /// a cidade com maior nome do Brasil possui 28 caracteres.
+        /// </summary>
+        /// 
+        public string Cidade
+        {
+            get
+            {
+                return this._Cidade;
+            }
+            set
+            {
+                if (value.Length > 30)
+                    throw new ArgumentException("O nome da cidade não pode exceder 30 caracteres!");
+                this._Cidade = value;
+            }
+        }
+
+        /// <summary>
+        /// A validação para o Bairro permite apenas strings
+        /// respeitando o limite máximo de 500 caracteres.        
+        /// </summary>
+        /// 
+        public string Bairro
+        {
+            get
+            {
+                return this._Bairro;
+            }
+            set
+            {
+                if (value.Length > 500)
+                    throw new ArgumentException("O nome da cidade não pode exceder 500 caracteres!");
+                this._Bairro = value;
+            }
+        }
+
+        /// <summary>
+        /// A validação para o estado permite apenas strings
+        /// respeitando o limite de 2 caracteres.        
+        /// A validação é feita através de um array de strings
+        /// que contem todas as siglas dos estados pertencentes
+        /// ao Brasil.
+        /// </summary>
+        /// 
+        public string UF
+        {
+            get
+            {
+                return this._Estado;
+            }
+            set
+            {
+                bool ok = false;
+                string[] estados =
+                {
+                    "AC","AL","AM","AP","BA","CE","DF","ES","GO","MA","MG","MS","MT","PA","PB","PE","PI","PR",
+                    "RJ","RN","RO","RR","RS","SC","SE","SP","TO"
+                };
+
+                foreach (string s in estados)
+                {
+                    if (s.ToUpper() == value)
+                    {
+                        ok = true;
+                        this._Estado = value;
+                    }
+                }
+
+                if (!ok)
+                    throw new ArgumentException("O estado informado não é um estado válido do Brasil!");
+            }
+        }
+
+        /// <summary>
+        /// Caso a cidade possua um CEP único para
+        /// todos os endereços, universalCep se torna verdadeiro.
+        /// </summary>
+        /// 
+        public bool universalCep { get; set; }
+    }
+}
---
+++ Correios.CEP/Correios.CEP/correiosCEP.cs
@@ -0,0 +1,71 @@
+﻿// Correios.CEP - API destinada a .NET com objetivo de realizar
+//a busca de endereços a partir do CEP através do site dos Correios.
+// Copyright (C) 2019 Murilo Rocha Peraira <mrex@tuta.io>
+// Copyright (C) 2019 Pereira Tech <mrex@tuta.io>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Correios.CEP
+{
+    public class correiosCEP
+    {
+        public static cepConsulta GetAddress(string cep)
+        {
+            var end = new cepConsulta();
+            end.Cep = cep;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm");
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            byte[] postBytes = Encoding.ASCII.GetBytes("relaxation=" + cep.Replace("-", "") + "&tipoCEP=ALL&semelhante=N");
+
+            request.GetRequestStream()
+                .Write(postBytes, 0, postBytes.Length);
+
+            string responseText = new StreamReader(request.GetResponse().GetResponseStream(), Encoding.GetEncoding("ISO-8859-1")).ReadToEnd();
+
+            string s = responseText.Substring(responseText.LastIndexOf("<tr>"));
+            s = s.Substring(0, s.LastIndexOf("</tr>"));
+            s = s.Replace("<tr>", "");
+            s = s.Replace("</tr>", "");
+            s = s.Replace("&nbsp;", "");
+            s = s.Replace("\r", "\n");
+
+            List<string> Address = new List<string>();
+            const string pattern = @"<td\b[^>]*?>(?<V>[\s\S]*?)</\s*td>";
+            foreach (Match match in Regex.Matches(s, pattern, RegexOptions.IgnoreCase))
+            {
+                string value = match.Groups["V"].Value;
+                Address.Add(value);
+            }
+
+            end.Rua = Address[0];
+            end.Bairro = Address[1];
+            end.Cidade = Address[2].Substring(0, Address[2].LastIndexOf("/"));
+            end.UF = Address[2].Substring(Address[2].LastIndexOf("/") + 1);
+
+            if (Address[0] == "")
+                end.universalCep = true;
+
+            return end;
+        }
+    }
+}
